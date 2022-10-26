4279b059e6ee64eb41efb8e1d61140951c86c761
zwula <348468470@qq.com>
2022-10-26 15:07:14 +0800

    feat: 封装request方法，并将其另存为模板方便之后项目复用

--- src/utils/requestTemplate.js
+++ src/utils/requestTemplate.js
@@ -2,6 +2,8 @@ import { notification } from "antd"
 import { history } from "umi"
 import { extend } from "umi-request"
 
+import { prefix } from "../service/config"
+
 /** 异常处理程序 */
 const codeMessage = {
   200: "服务器成功返回请求的数据。",
@@ -43,20 +45,55 @@ const errorHandler = (error) => {
 
 //对 extend 实例进行简单的封装
 const request = extend({
+  // 'params' 是即将于请求一起发送的 URL 参数，参数会自动 encode 后添加到 URL 中
+  // 类型需为 Object 对象或者 URLSearchParams 对象
+  params: { id: 1 },
+  // 'paramsSerializer' 开发者可通过该函数对 params 做序列化（注意：此时传入的 params 为合并了 extends 中 params 参数的对象，如果传入的是 URLSearchParams 对象会转化为 Object 对象
+  paramsSerializer: function (params) {
+    return Qs.stringify(params, { arrayFormat: "brackets" })
+  },
+  // 'data' 作为请求主体被发送的数据
+  // 适用于这些请求方法 'PUT', 'POST', 和 'PATCH'
+  // 必须是以下类型之一：
+  // - string, plain object, ArrayBuffer, ArrayBufferView, URLSearchParams
+  // - 浏览器专属：FormData, File, Blob
+  // - Node 专属： Stream
+  data: { name: "Mike" },
+
   //  请求头，报文首部-首部字段
   headers: {
     // headers中搭载需要携带的首部信息
-    "content-type": "application/json",
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authentication: localStorage.getItem("token") || "",
   },
   // 'timeout' 指定请求超时的毫秒数（0 表示无超时时间）
   timeout: 3000,
-  // 'credentials' 发送带凭据的请求
+  // 'prefix' 前缀，统一设置 url 请求前缀
+  // ( e.g. request('/user/save', { prefix: '/api/v1' }) => request('/api/v1/user/save') )
+  prefix: prefix,
+  // 'suffix' 后缀，统一设置 url 后缀
+  // ( e.g. request('/api/v1/user/save', { suffix: '.json'}) => request('/api/v1/user/save.json') )
+  suffix: "",
+  // 'credentials' 发送带凭据(cookie)的请求
   // 为了让浏览器发送包含凭据的请求（即使是跨域源），需要设置 credentials: 'include'
   // 如果只想在请求URL与调用脚本位于同一起源处时发送凭据，请添加credentials: 'same-origin'
   // 要改为确保浏览器不在请求中包含凭据，请使用credentials: 'omit'
   credentials: "same-origin", // default
   // 开启CORS跨域
   crossOrigin: true,
+  // 'useCache' 是否使用缓存，当值为 true 时，GET 请求在 ttl 毫秒内将被缓存，缓存策略唯一 key 为 url + params 组合
+  useCache: false, // default
+  // 'ttl' 缓存时长（毫秒）， 0 为不过期
+  ttl: 60000,
+  // 'maxCache' 最大缓存数， 0 为无限制
+  maxCache: 0,
+  // 'charset' 当服务端返回的数据编码类型为 gbk 时可使用该参数，umi-request 会按 gbk 编码做解析，避免得到乱码, 默认为 utf8
+  // 当 parseResponse 值为 false 时该参数无效
+  charset: "gbk",
+  // 'responseType': 如何解析返回的数据，当 parseResponse 值为 false 时该参数无效
+  // 默认为 'json', 对返回结果进行 Response.text().then( d => JSON.parse(d) ) 解析
+  // 其他(text, blob, arrayBuffer, formData), 做 Response[responseType]() 解析
+  responseType: "json", // default
   //处理请求错误 调用上面的错误处理逻辑
   errorHandler: errorHandler,
 })
@@ -86,5 +123,3 @@ request.interceptors.response.use(async (response) => {
   }
   return response
 })
-
-export default request
